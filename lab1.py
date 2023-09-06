adf9d97888d29efc3a30a014a5d35fbc6e62f456
Snail <59337566+ToxicSnail@users.noreply.github.com>
2023-09-06 17:43:41 +0300

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,29 +1,31 @@
 import threading
 import time
 
-event = threading.Event()
+event = threading.Event()    # Создаем объект события
 
-def producer():
+def producer():    # Функция потока-поставщика
     while True:
         print("Поток-поставщик:инициализированно условное событие")
-        event.set()
+        event.set()    # Устанавливаем событие
         time.sleep(1)
 
-def consumer():
+def consumer():    # ункция потока-потребителя
     while True:
         print("Поток-потребитель: ожидание условного события")
-        event.wait()
-        event.clear()
+        event.wait()    # Ожидаем событие
+        event.clear()    # Сбрасываем наше событие
         print("Поток-потребитель: условное событие получено")
 
+# Создаем и запускаем потоки
 producer_thread = threading.Thread(target=producer)
 consumer_thread = threading.Thread(target=consumer)
 
 producer_thread.start()
 consumer_thread.start()
 
+# Чтобы завершить программу, можно использовать KeyboardInterrupt (Ctrl+C)
 try:
     producer_thread.join()
     consumer_thread.join()
 except KeyboardInterrupt:
-    print("Программа завершена")
+    print("Программа завершена!")
