fe985dcd871ded3cd5870c6d9da880e6df033bd6
Snail <59337566+ToxicSnail@users.noreply.github.com>
2023-09-21 17:11:41 +0300

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -1,87 +1,93 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
 #include <signal.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
-#include <netinet/in.h> // Add this include for struct sockaddr_in
-#include <time.h>       // Add this include for struct timespec
 
-#define PORT 8080
-#define MAX_CONNECTIONS 5
+volatile sig_atomic_t keep_running = 1;
 
-volatile sig_atomic_t is_running = 1;
-
-void signal_handler(int signo) {
+void handle_signal(int signo) {
     if (signo == SIGHUP) {
         printf("Received SIGHUP signal\n");
-        // Additional handling for SIGHUP (as needed)
     }
+    keep_running = 0;
 }
 
-int main() {
-    // Set up a signal handler for SIGHUP
-    struct sigaction sa;
-    memset(&sa, 0, sizeof(sa));
-    sa.sa_handler = signal_handler;
-    sigaction(SIGHUP, &sa, NULL);
-
-    // Create a socket
-    int server_socket;
-    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-        perror("Socket creation failed");
-        exit(EXIT_FAILURE);
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+        return 1;
     }
 
-    struct sockaddr_in server_addr;
+    int server_sock, client_sock;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t client_addr_len = sizeof(client_addr);
+    int port = atoi(argv[1]);
+
+    // Create socket
+    if ((server_sock = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("Socket creation failed");
+        return 1;
+    }
+
+    // Initialize server address structure
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(port);
 
-    // Bind the socket to the address and port
-    if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+    // Bind socket to the specified port
+    if (bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
         perror("Bind failed");
-        exit(EXIT_FAILURE);
+        close(server_sock);
+        return 1;
     }
 
-    // Start listening on the port
-    if (listen(server_socket, MAX_CONNECTIONS) == -1) {
+    // Listen for incoming connections
+    if (listen(server_sock, 5) == -1) {
         perror("Listen failed");
-        exit(EXIT_FAILURE);
+        close(server_sock);
+        return 1;
     }
 
-    fd_set read_fds, temp_fds;
-    FD_ZERO(&read_fds);
-    FD_SET(server_socket, &read_fds);
-    int max_fd = server_socket;
+    // Register signal handler for SIGHUP
+    signal(SIGHUP, handle_signal);
 
-    while (is_running) {
-        temp_fds = read_fds;
+    printf("Server is running on port %d\n", port);
 
-        struct timespec timeout;
-        timeout.tv_sec = 1;  // Wait for 1 second
-        timeout.tv_nsec = 0;
-
-        int result = pselect(max_fd + 1, &temp_fds, NULL, NULL, &timeout, NULL);
-        if (result == -1) {
-            perror("pselect");
+    while (keep_running) {
+        // Accept a new client connection
+        if ((client_sock = accept(server_sock, (struct sockaddr *)&client_addr, &client_addr_len)) == -1) {
+            perror("Accept failed");
             continue;
-        } else if (result == 0) {
-            // Timeout occurred, you can perform some actions here if needed
         }
 
-        for (int fd = 0; fd <= max_fd; fd++) {
-            if (FD_ISSET(fd, &temp_fds)) {
-                // Handle incoming connections and data from clients
-                // ...
-            }
+        // Log the new connection
+        char client_ip[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, sizeof(client_ip));
+        printf("Accepted connection from %s:%d\n", client_ip, ntohs(client_addr.sin_port));
+
+        // Read and process data from the client
+        char buffer[1024];
+        ssize_t bytes_read;
+        while ((bytes_read = recv(client_sock, buffer, sizeof(buffer), 0)) > 0) {
+            printf("Received %zd bytes from the client\n", bytes_read);
+            // You can process the data here as needed
         }
+
+        if (bytes_read == -1) {
+            perror("Recv failed");
+        }
+
+        // Close the client socket
+        close(client_sock);
     }
 
-    // Close the server socket and free resources
-    close(server_socket);
-
+    // Close the server socket
+    close(server_sock);
+    printf("Server is shutting down\n");
     return 0;
 }
