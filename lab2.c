b21f2901d9ece6d57d98f6d49b47821742e6b7fe
Snail <59337566+ToxicSnail@users.noreply.github.com>
2023-09-21 17:31:56 +0300

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -27,66 +27,66 @@ int main(int argc, char *argv[]) {
     socklen_t client_addr_len = sizeof(client_addr);
     int port = atoi(argv[1]);
 
-    // Create socket
+    // Создание сокета
     if ((server_sock = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
         perror("Socket creation failed");
         return 1;
     }
 
-    // Initialize server address structure
+    // Инициализация структуры адресов сервера
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(port);
 
-    // Bind socket to the specified port
+    // Привязка сокета к указанному порту
     if (bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
         perror("Bind failed");
         close(server_sock);
         return 1;
     }
 
-    // Listen for incoming connections
+    // Прослушивание входящего соединения
     if (listen(server_sock, 5) == -1) {
         perror("Listen failed");
         close(server_sock);
         return 1;
     }
 
-    // Register signal handler for SIGHUP
+    // Регистрация обработчика сигнала SIGHUP
     signal(SIGHUP, handle_signal);
 
     printf("Server is running on port %d\n", port);
 
     while (keep_running) {
-        // Accept a new client connection
+        // Прием нового клиентского соединения
         if ((client_sock = accept(server_sock, (struct sockaddr *)&client_addr, &client_addr_len)) == -1) {
             perror("Accept failed");
             continue;
         }
 
-        // Log the new connection
+        // Регистрация нового соединения
         char client_ip[INET_ADDRSTRLEN];
         inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, sizeof(client_ip));
         printf("Accepted connection from %s:%d\n", client_ip, ntohs(client_addr.sin_port));
 
-        // Read and process data from the client
+        // Считывание и обработка данных от клиента
         char buffer[1024];
         ssize_t bytes_read;
         while ((bytes_read = recv(client_sock, buffer, sizeof(buffer), 0)) > 0) {
             printf("Received %zd bytes from the client\n", bytes_read);
-            // You can process the data here as needed
+            //printf("Test bytes catched");
         }
 
         if (bytes_read == -1) {
             perror("Recv failed");
         }
 
-        // Close the client socket
+        // Закрыть серверный сокет
         close(client_sock);
     }
 
-    // Close the server socket
+    // Закрыть серверный сокет
     close(server_sock);
     printf("Server is shutting down\n");
     return 0;
