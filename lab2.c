5d34901b4bc4000ceb4434e24052fc985a894f9d
Горький Кирилл Дмитриевич <59337566+ToxicSnail@users.noreply.github.com>
2023-10-14 13:08:00 +0300

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>    //close для сокета и для sleep
+#include <unistd.h>
 #include <signal.h>
-#include <sys/socket.h>    //для работы с сокетами
-#include <netinet/in.h>    //для работы с сетевыми адресами 
-#include <arpa/inet.h>    //для работы с IP-адресами
-#include <sys/select.h>    //для работы pselect
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/select.h>
 #include <time.h>
+#include <errno.h>
 
 volatile sig_atomic_t keep_running = 1;
 
@@ -25,59 +26,65 @@ int main(int argc, char *argv[]) {
     int server_sock, client_sock;
     struct sockaddr_in server_addr, client_addr;
     socklen_t client_addr_len = sizeof(client_addr);
-    int port = atoi(argv[1]);    //преобр строки в число
+    int port = atoi(argv[1]);
 
-    // Создание сокета
+    // Create socket
     if ((server_sock = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
         perror("Socket creation failed");
         return 1;
     }
 
-    // Инициализация структуры адресов сервера
+    // Initialize server address structure
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(port);
 
-    // Привязка сокета к указанному порту
+    // Bind socket to the specified port
     if (bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
         perror("Bind failed");
         close(server_sock);
         return 1;
     }
 
-    // Прослушивание входящего соединения
+    // Listen for incoming connections
     if (listen(server_sock, 5) == -1) {
         perror("Listen failed");
         close(server_sock);
         return 1;
     }
 
-    // Регистрация обработчика сигнала SIGHUP
+    // Register signal handler for SIGHUP
     signal(SIGHUP, handle_signal);
 
     printf("Server is running on port %d\n", port);
 
-    fd_set readfds;
-    FD_ZERO(&readfds);
-    FD_SET(server_sock, &readfds);
-    int max_fd = server_sock; // Текущее максимальное значение дескриптора сокса
+    
 
     struct timespec timeout = {5, 0}; // 5 seconds
-    sigset_t original_mask; // Задаем исходную маску блокирования сигналов
+    sigset_t blockedMask, origMask;
 
-    // Задаем исходную маску блокирования сигналов
-    sigemptyset(&original_mask);
-    sigaddset(&original_mask, SIGHUP);
+    sigemptyset(&blockedMask);
+    sigaddset(&blockedMask, SIGHUP);
+
+    // Заблокировать сигнал SIGHUP
+    if (sigprocmask(SIG_BLOCK, &blockedMask, &origMask) == -1) {
+        perror("sigprocmask error");
+        return 1;
+    }
 
     while (keep_running) {
+        fd_set readfds;
+        FD_ZERO(&readfds);
+        FD_SET(server_sock, &readfds);
+        int max_fd = server_sock;
         // Копируем набор дескрипторов, так как select изменяет его
         fd_set temp_fds = readfds;
 
-        //Ожидаем активность на сокете с использованием pselect
+        // Wait for activity on sockets using pselect
         int ready_fds;
         do {
-            ready_fds = pselect(max_fd + 1, &temp_fds, NULL, NULL, &timeout, &original_mask);
+            ready_fds = pselect(max_fd + 1, &temp_fds, NULL, NULL, &timeout, &origMask);
         } while (ready_fds == -1 && errno == EINTR);
 
         if (ready_fds == -1) {
@@ -86,17 +93,17 @@ int main(int argc, char *argv[]) {
         } else if (ready_fds == 0) {
             printf("No activity on sockets\n");
         } else {
-            // Проверка наличия входящих соединений
+            // Проверка входящих соединений
             if (FD_ISSET(server_sock, &temp_fds)) {
                 if ((client_sock = accept(server_sock, (struct sockaddr *)&client_addr, &client_addr_len)) == -1) {
                     perror("Accept failed");
                 } else {
-                    // Регистрация нового соединения
+                    // Логгирование нового соединения
                     char client_ip[INET_ADDRSTRLEN];
                     inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, sizeof(client_ip));
                     printf("Accepted connection from %s:%d\n", client_ip, ntohs(client_addr.sin_port));
 
-                    // Добавить клиентский сокет в набор
+                    // Добавление сокса клиента в множество
                     FD_SET(client_sock, &readfds);
 
                     // Обновление max_fd при необходимости
@@ -106,7 +113,7 @@ int main(int argc, char *argv[]) {
                 }
             }
 
-            // Проверка наличия данных на сокетах клиентов
+            // Проверка данных на соксах клиентов
             for (int i = server_sock + 1; i <= max_fd; i++) {
                 if (FD_ISSET(i, &temp_fds)) {
                     char buffer[1024];
@@ -115,26 +122,27 @@ int main(int argc, char *argv[]) {
                     if (bytes_read == -1) {
                         perror("Recv failed");
                     } else if (bytes_read == 0) {
-                        // Соединение закрывается клиентом
+                        // Соединение закрыто клиентом
                         printf("Connection closed by client\n");
                         close(i);
                         FD_CLR(i, &readfds);
                     } else {
                         printf("Received %zd bytes from client\n", bytes_read);
+                        // Обработка данных, если необходимо
                     }
                 }
             }
         }
     }
 
-    // Закрыть серверный сокет
+    // Закрытие всех соксов клиентов
     for (int i = server_sock + 1; i <= max_fd; i++) {
         if (FD_ISSET(i, &readfds)) {
             close(i);
         }
     }
 
-    // Закрыть серверный сокет
+    // Закрытие сокса сервера
     close(server_sock);
     printf("Server is shutting down\n");
     return 0;
